9b79c27cd7ec1c1b5482c2795eb79458afc7e594
Nicolás Barona Riera <nbaronariera@gmail.com>
2025-02-26 14:18:54 +0100

    Incio de Spring Initr

---
+++ src/test/java/com/nombre/SolidarityHub/SolidarityHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nombre.SolidarityHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidarityHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
