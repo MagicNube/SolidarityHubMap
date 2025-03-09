4c8f5b56a76787ecccd646d64bae7b3c6d2126e4
Jesús Caravaca <92930516+MagicNube@users.noreply.github.com>
2025-03-09 12:05:12 +0100

    prueba2 (#5)
    * Test: Tremendo contenido
    
    * ADD new comment
    
    * Hola
    
    * primer Commit
    
    * Nuevo html
    
    * Warning
    
    ---------
    
    Co-authored-by: Nicolás Barona Riera <nbaronariera@gmail.com>
    Co-authored-by: Francisco Blanco <17frablamig@iesvelesevents.com>
    Co-authored-by: Saul Alcazar Lopez <SALCLOP@upv.edu.es>

--- src/main/java/es/pingu/map/MapApplication.java
+++ src/main/java/es/pingu/map/MapApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MapApplication {
 
+	// Testeo testeillo version 2
+	//testeitooooo
+	//No hacer esto en casa
 	public static void main(String[] args) {
 		SpringApplication.run(MapApplication.class, args);
 	}
