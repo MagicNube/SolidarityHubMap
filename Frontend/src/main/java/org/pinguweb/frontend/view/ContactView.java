3d41b3ea7d476a1ea74c854349fd3ee264e9c71c
Nicolás Barona Riera <nbaronariera@gmail.com>
2025-04-01 13:14:08 +0200

    Feat: Textito

--- Frontend/src/main/java/org/pinguweb/frontend/view/ContactView.java
+++ Frontend/src/main/java/org/pinguweb/frontend/view/ContactView.java
@@ -1,6 +1,7 @@
 package org.pinguweb.frontend.view;
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.router.Route;
 
 @Route("contact")
@@ -9,5 +10,10 @@ public class ContactView extends VerticalLayout {
     public ContactView() {
         this.setSizeFull();
         this.add(NavigationBar.createNavBar());
+        TextArea area = new TextArea();
+        area.setLabel("Conctacta con nosotros usando el correo:" +
+                "nbarrie@upv.edu.es");
+
+        this.add(area);
     }
 }
