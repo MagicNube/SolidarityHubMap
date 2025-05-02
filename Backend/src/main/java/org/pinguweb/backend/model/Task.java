bdf88d73a07234f94fca9e9d101d699f7fc54900
FranBlanco2003 <17frablamig@gmail.com>
2025-05-02 22:25:27 +0200

    fix(back)2

--- Backend/src/main/java/org/pinguweb/backend/model/Task.java
+++ Backend/src/main/java/org/pinguweb/backend/model/Task.java
@@ -61,7 +61,6 @@ public class Task {
     @ManyToOne(cascade = CascadeType.ALL)
     private Zone zone;
 
-
     public Task(Need need, String taskName, String taskDescription, LocalDateTime startTimeDate,
                 LocalDateTime estimatedEndTimeDate, Priority priority, Status status, Volunteer volunteer, GPSCoordinates gpsCoordinates) {
         this.need = List.of(need);
