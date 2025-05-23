e2636b0262599068b75c38790ffc4fefd9dae9e4
Saul Alcazar Lopez <SALCLOP@upv.edu.es>
2025-05-23 13:48:28 +0200

    Ahora debería poderse coger de la BD resource y Donaciones

---
+++ Backend/src/main/java/org/pinguweb/backend/controller/ResourceController.java
@@ -0,0 +1,43 @@
+package org.pinguweb.backend.controller;
+
+import org.pingu.domain.DTO.ResourceDTO;
+import org.pingu.domain.DTO.factories.BackendDTOFactory;
+import org.pingu.domain.DTO.factories.ModelDTOFactory;
+import org.pingu.persistence.service.ResourceService;
+import org.pinguweb.backend.controller.common.ServerException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+public class ResourceController {
+
+    @Autowired
+    ResourceService service;
+    @Autowired
+    BackendDTOFactory factory;
+    @Autowired
+    ModelDTOFactory modelFactory;
+
+    @Async
+    @GetMapping("/resources")
+    public CompletableFuture<ResponseEntity<List<ResourceDTO>>> getAll(){
+       if (ServerException.isServerClosed(service.getResourceRepository())){
+              return CompletableFuture.completedFuture(ResponseEntity.internalServerError().build());
+         }
+
+          List<ResourceDTO> resources = service.findAll().stream()
+                 .map(factory::createDTO)
+                 .collect(Collectors.toList());
+
+          return CompletableFuture.completedFuture(ResponseEntity.ok(resources));
+       }
+
+}
+
+
+
