caa1d88c02d49401adc7d7aabd5296d08047d3bd
Nicolás Barona Riera <nbaronariera@gmail.com>
2025-05-24 10:32:05 +0200

    Fix: arreglos del back

--- Backend/src/main/java/org/pinguweb/backend/controller/DonationController.java
+++ Backend/src/main/java/org/pinguweb/backend/controller/DonationController.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 import java.util.List;
@@ -17,6 +19,9 @@ import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
+
+@RestController
+@RequestMapping("/api")
 public class DonationController {
 
     @Autowired
--- Backend/src/main/java/org/pinguweb/backend/controller/ResourceController.java
+++ Backend/src/main/java/org/pinguweb/backend/controller/ResourceController.java
@@ -9,11 +9,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
+@RestController
+@RequestMapping("/api")
 public class ResourceController {
 
     @Autowired
